436078b607dc210d7e5a3a8f687ff1916dec0b40
Sarah Hong <64706876+srxsyd@users.noreply.github.com>
2026-06-22 18:17:25 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> prefix(nums.size());
+        vector<int> suffix(nums.size());
+
+        int pre = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            prefix[i] = pre;
+            pre *= nums[i];
+        }
+
+        int suf = 1;
+        for (int i = nums.size()-1; i >= 0; i--) {
+            suffix[i] = suf;
+            suf *= nums[i];
+        }
+
+        vector<int> res(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] = prefix[i] * suffix[i];
+        }
+
+        return res;
+    }
+};
